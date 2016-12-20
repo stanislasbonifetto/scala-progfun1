87e5ad9b307e41ac9145d50335ed7b6056e1c1dd
Stanislas Bonifetto <stanislas.bonifetto@gmail.com>
2016-12-19 22:44:58 -0700

    natural number

---
+++ src/main/scala/it/stanislas/scala/progfun1/week4/Nat.scala
@@ -0,0 +1,31 @@
+package it.stanislas.scala.progfun1.week4
+
+/**
+  * Created by stanislas on 19/12/2016.
+  */
+//Peano numbers
+abstract class Nat {
+  def isZero: Boolean
+  def predecessor: Nat
+  def successor: Nat = new Succ(this)
+  def + (that: Nat): Nat
+  def - (that: Nat): Nat
+}
+object Zero extends Nat{
+  override def isZero: Boolean = true
+
+  override def predecessor: Nat = throw new Exception("0.predecessor")
+
+  override def +(that: Nat): Nat = that
+
+  override def -(that: Nat): Nat = if (that.isZero) this else throw new Exception("negative number")
+}
+class Succ(n: Nat) extends Nat{
+  override def isZero: Boolean = false
+
+  override def predecessor: Nat = n
+
+  override def +(that: Nat): Nat = new Succ(n + that)
+
+  override def -(that: Nat): Nat = if(that.isZero) this else n - that.predecessor
+}

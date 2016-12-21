68edfd7968dd33bf3c85f5447ca1f1d232ed62a6
Stanislas Bonifetto <stanislas.bonifetto@gmail.com>
2016-12-21 00:32:05 -0700

    create product and var

--- src/main/scala/it/stanislas/scala/progfun1/week4/Expr.scala
+++ src/main/scala/it/stanislas/scala/progfun1/week4/Expr.scala
@@ -6,8 +6,24 @@ package it.stanislas.scala.progfun1.week4
 trait Expr {
   def eval: Int = this match {
     case Number(n) => n
-    case Sum(e1, e2) => e1.eval + e2.eval
+    case Sum(l, r) => l.eval + r.eval
+  }
+
+  def show: String = {
+    def parenthesis(e: Expr):String = e match {
+      case Sum(_, _) => "("+ e.show + ")"
+      case _ => e.show
+    }
+
+    this match {
+      case Number(x) => x.toString
+      case Sum(l, r) => l.show + " + " + r.show
+      case Var(v) => v
+      case Prod(l ,r) => parenthesis(l) + " * " + parenthesis(r)
+    }
   }
 }
 case class Number(n: Int) extends Expr
-case class Sum(e1: Expr, e2: Expr) extends Expr
+case class Sum(l: Expr, r: Expr) extends Expr
+case class Var(v: String) extends Expr
+case class Prod(l: Expr, r: Expr) extends Expr
--- src/main/scala/it/stanislas/scala/progfun1/week4/exprs.sc
+++ src/main/scala/it/stanislas/scala/progfun1/week4/exprs.sc
@@ -1,10 +1,13 @@
 import it.stanislas.scala.progfun1.week4._
 
 object exprs {
-  def show(e: Expr):String = e match {
-    case Number(x) => x.toString
-    case Sum(l, r) => show(l) + " + " + show(r)
-  }
 
-  show(Sum(Number(1), Number(5)))
+
+
+
+  Sum(Number(1), Number(5)).show
+
+  Sum(Prod(Number(2), Var("x")), Var("y")).show
+
+  Prod(Sum(Number(2), Var("x")), Var("y")).show
 }
